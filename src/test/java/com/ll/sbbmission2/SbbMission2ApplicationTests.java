b156545d426448d13e5a5f3636e99af6c6b5d383
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-30 14:44:40 +0900

    2-5 답변조회 테스트

--- src/test/java/com/ll/sbbmission2/SbbMission2ApplicationTests.java
+++ src/test/java/com/ll/sbbmission2/SbbMission2ApplicationTests.java
@@ -1,17 +1,15 @@
 package com.ll.sbbmission2;
 
 import com.ll.sbbmission2.Entity.Answer;
-import com.ll.sbbmission2.Entity.Question;
 import com.ll.sbbmission2.Repository.AnswerRepository;
 import com.ll.sbbmission2.Repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SbbMission2ApplicationTests {
@@ -24,16 +22,10 @@ class SbbMission2ApplicationTests {
     @Test
     void testJpa() {
 
-        Optional<Question> oq = this.questionRepository.findById(2); // DB 에서 질문 아이디 에 대한 데이터를 찾아 oq 에 담는다
-        assertTrue(oq.isPresent()); // 값이 존재한다면 true 반환 , 아니면 false
-        Question q = oq.get(); // 값을 불러 온다
-
-        Answer a = new Answer();
-
-        a.setContent("네 자동으로 생성됩니다."); // 답변에 저장 할 내용
-        a.setQuestion(q); //어떤 질문의 답변인기 알기위해서 Question 객체가 필요
-        a.setCreateDate(LocalDateTime.now());
-        this.answerRepository.save(a); //답변저장
+        Optional<Answer> oa = this.answerRepository.findById(1); //답변 id 가 1 인데이터를 찾아 oa 담는다
+        assertTrue(oa.isPresent()); //값을 존재하는지 확인한다.
+        Answer a= oa.get();
+        assertEquals(2, a.getQuestion().getId()); //질문 아이디 값이 2인지 확인한다
     }
 
 }
