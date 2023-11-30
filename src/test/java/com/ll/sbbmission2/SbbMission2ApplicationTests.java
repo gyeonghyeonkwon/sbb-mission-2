fd937edd4023d82be86689c448825962a11d1b46
gyeonghyeonkwon <kyanghyang12@naver.com>
2023-11-30 14:31:44 +0900

    2-5 답변생성 테스트

--- src/test/java/com/ll/sbbmission2/SbbMission2ApplicationTests.java
+++ src/test/java/com/ll/sbbmission2/SbbMission2ApplicationTests.java
@@ -1,14 +1,16 @@
 package com.ll.sbbmission2;
 
+import com.ll.sbbmission2.Entity.Answer;
 import com.ll.sbbmission2.Entity.Question;
+import com.ll.sbbmission2.Repository.AnswerRepository;
 import com.ll.sbbmission2.Repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -16,15 +18,22 @@ class SbbMission2ApplicationTests {
 
     @Autowired //해당 클래스에서 db 를 사용하기위함
     QuestionRepository questionRepository;
+
+    @Autowired
+    AnswerRepository answerRepository;
     @Test
     void testJpa() {
 
-        assertEquals(2,this.questionRepository.count()); //데이터가 2개인지 확인
-        Optional<Question> oq = this.questionRepository.findById(1);// question 객체의 id값을 db 에서 찾는다
-        assertTrue(oq.isPresent()); //1번 id 값이 존재 여부 확인
-        Question q = oq.get();// 1번 값을 가져온다.
-        this.questionRepository.delete(q); // 가져온 1번값을 삭제한다.
-        assertEquals(1,this.questionRepository.count()); // 삭제 한 후 데이터가 1개 인지 확인
+        Optional<Question> oq = this.questionRepository.findById(2); // DB 에서 질문 아이디 에 대한 데이터를 찾아 oq 에 담는다
+        assertTrue(oq.isPresent()); // 값이 존재한다면 true 반환 , 아니면 false
+        Question q = oq.get(); // 값을 불러 온다
+
+        Answer a = new Answer();
+
+        a.setContent("네 자동으로 생성됩니다."); // 답변에 저장 할 내용
+        a.setQuestion(q); //어떤 질문의 답변인기 알기위해서 Question 객체가 필요
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a); //답변저장
     }
 
 }
